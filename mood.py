f6d8a5d5c8dc2797791a473425ddeb07efafe26b
PERZI0N <vaidik.chhirolya1@gmail.com>
2022-08-11 17:28:25 +0530

    making the requires xlsx
    made all the databases  for all kinds of genres

--- mood.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-df = pd.read_excel("final.xlsx")
-genre = ["Action","Horror","Thriller","Drama","Biopic","Fantasy","Sci/Fi","Comedy","Animated","Romantic"]
-writer = pd.ExcelWriter("Mood.xlsx")
-for i in range(2,df['title'].size):
-    if(df.iloc[i]['Genre']):
-        df.iloc[i][]
-print(df.iloc[5]['title'])
