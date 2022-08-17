90429e6af994340d3341db1121ce165ee727deeb
Sanika-Chavan <98613739+Sanika-Chavan@users.noreply.github.com>
2022-08-17 16:48:20 +0530

    Updated question pages
    CSS work remaining

--- FrontEnd/Main.js
+++ FrontEnd/Main.js
@@ -1,3 +1,29 @@
 document.getElementById("start").onclick=function(){
     location.href = 'Q1.html';
 };
+
+document.getElementById("Q1Next").onclick=function(){
+    location.href = 'Q2.html';
+};
+
+// Previous Buttons
+
+document.getElementById("Q1Pre").onclick=function(){
+    location.href = 'Main.html';
+};
+
+document.getElementById("Q2Pre").onclick=function(){
+    location.href = 'Q1.html';
+};
+
+document.getElementById("Q3Pre").onclick=function(){
+    location.href = 'Q2.html';
+};
+
+document.getElementById("Q4Pre").onclick=function(){
+    location.href = 'Q3.html';
+};
+
+document.getElementById("Q5Pre").onclick=function(){
+    location.href = 'Q4.html';
+};
